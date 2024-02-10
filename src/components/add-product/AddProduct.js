1b3bf0cacd5191bb2398ff0e0302f82db416ee66
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-02-10 11:37:12 +0530

    consoling product info

--- src/components/add-product/AddProduct.js
+++ src/components/add-product/AddProduct.js
@@ -1,23 +1,44 @@
+import { useState, useRef } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./AddProduct.module.css";
 
 const AddProduct = (props) => {
+  const productIdRef = useRef()
+  const sellingPriceRef = useRef()
+  const productNameRef = useRef()
+  const categoryRef = useRef()
+
+  const onSubmitHandler=event=>{
+    event.preventDefault()
+    const productInfo = {
+      productId : productIdRef.current.value,
+      sellingPrice : sellingPriceRef.current.value,
+      productName : productNameRef.current.value,
+      category : categoryRef.current.value
+    }
+
+    console.log(productInfo)
+  }
   return (
     <Card className={styles.input}>
-      <form >
+      <form onSubmit={onSubmitHandler}>
         <label htmlFor="product-id">Product ID</label>
-        <input id="product-id" type="text" />
+        <input id="product-id" type="text" ref={productIdRef}/>
+
         <label htmlFor="selling-price">Selling price</label>
-        <input id="selling-price" type="number" min={0.01} step={0.01} />
+        <input ref={sellingPriceRef} id="selling-price" type="number" min={0.01} step={0.01} />
+
         <label htmlFor="product-name">Product name</label>
-        <input id="product-name" type="text" />
+        <input ref={productNameRef} id="product-name" type="text" />
+
         <label htmlFor="category">Choose category</label>
-        <select id="category">
-          <option value="volvo">Electronics</option>
-          <option value="saab">Food item</option>
-          <option value="mercedes">Skin care</option>
+        <select ref={categoryRef} id="category">
+          <option value="Electronics">Electronics</option>
+          <option value="Food Item">Food item</option>
+          <option value="Skin care">Skin care</option>
         </select>
+
         <Button type="submit">Add product</Button>
       </form>
     </Card>
